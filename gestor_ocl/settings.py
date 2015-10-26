85581ab49ce17daffe96b35a5226be6cbb045cdf
Néstor Manuel <nestor706@gmail.com>
2015-10-26 16:44:37 -0430

    BASE_DIR modification to repair failing collectstatic

--- gestor_ocl/settings.py
+++ gestor_ocl/settings.py
@@ -17,7 +17,7 @@ import os
 import dj_database_url
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
