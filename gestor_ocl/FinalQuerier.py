50ab0aadfa3c278c85d718311c9a8cab0a99efd9
skydaddy <omega31388>
2016-02-18 10:36:12 -0430

    Queries finalizados. LIKE POWER

--- gestor_ocl/FinalQuerier.py
+++ gestor_ocl/FinalQuerier.py
@@ -1,9 +1,22 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
+
 from rdflib import plugin, Graph, Literal, URIRef
 from rdflib.store import Store
-from rdflib.namespace import RDF, RDFS
 
+
+def db_starter(pgmaker):
+    ident = URIRef('Adm-Publi.rdfs')
+    store = plugin.get('SQLAlchemy', Store)(identifier=ident)
+    g = Graph(store, identifier=ident)
+    dburi = Literal('postgresql+psycopg2://skydaddy:tesis2015@localhost:5432/ocl_manager')
+    if pgmaker == 1:
+        g.open(dburi, create=True)
+        g.parse('Adm-Publi.rdfs')
+        g.parse('Adm-Publi.rdf')
+    else:
+        g.open(dburi, create=False)
+    return g
 
 def querylvl1(option, graph):
     g = graph
